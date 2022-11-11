aa25f7283c4906eaea405bccedb6277353da9cc6
Rustem <roustem.fa@yandex.ru>
2022-11-11 15:25:04 +0300

    upload files

---
+++ script.js
@@ -0,0 +1,51 @@
+const canvas = document.querySelector('#canvas1')
+const ctx = canvas.getContext('2d')
+CANVAS_WIDTH = canvas.width = 900
+CANVAS_HEIGHT = canvas.height = 1000
+const numberOfEnemies = 100
+const enemiesArray =[]
+const enemyImage = new Image
+enemyImage.src = 'images/enemy1.png'
+let getFrame = 0
+class Enemy {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;        
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+        
+
+    }
+    update(){
+        this.x += this.speed;
+        this.y += this.speed;
+        if (getFrame %  this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++
+        }
+    }
+    draw(){
+        //ctx.drawImage(crop of sorce Image,/ area on Canvas) 
+        ctx.drawImage(enemyImage, this.frame * this.spriteWidth , 0, this.spriteWidth, this.spriteHeight,
+             this.x, this.y, this.width, this.height)               
+    }
+}
+
+for (let i=0; i <= numberOfEnemies; i++){
+    enemiesArray.push(new Enemy())
+ 
+}
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)    
+    enemiesArray.forEach((enemy) => {
+        enemy.update()
+        enemy.draw()
+    })
+    getFrame++
+    requestAnimationFrame(animate)
+}
+animate()
